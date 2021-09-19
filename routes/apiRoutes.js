4e2d4850df3974d43ed778b50d0759a98307642b
Luke Teal <l.teal21@gmail.com>
2021-09-19 19:24:10 -0400

    Add get Dispositions route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const {User, Disposition} = require('../models')
 function apiRoutes(app) {
     // Post Route - Disposition
     app.post('/api/disposition', (req, res) => {
-        console.log(req.body)
+        // console.log(req.body)
         Disposition.create(req.body)
         .then(disposition => {
             res.send('Completed')
@@ -11,7 +11,13 @@ function apiRoutes(app) {
     })
 
     // Get Route
-
+    app.get('/api/dispositions', (req, res) => {
+        // console.log(req.body)
+        Disposition.findAll({include: User})
+        .then(dispositions => {
+            res.send(dispositions)
+        })
+    })
 }
 
 module.exports = apiRoutes
